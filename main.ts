31436f7e80eb467ebc1066ba1c109a9c27c85138
Mason-tom <mason.tom@stu.ocsb.ca>
2026-01-08 13:27:39 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Mason
+ * Created on: Sep 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let motor: stepperMotor.Motor
+
+//setup
+basic.showString("READY")
+robotbit.MotorRun(robotbit.Motors.M1A, 150)
+
+// if button A is pressed, LEFT wheel spins foraward
+input.onButtonPressed(Button.A, function () {
+    robotbit.StepperDual(2880, 2880)
+    basic.pause(100)
+})
